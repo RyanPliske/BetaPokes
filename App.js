271efc842731eca0d6af296defd8f861de808286
Ryan Pliske <ryan.pliske@monsanto.com>
2019-03-30 18:29:16 -0500

    start off map at user's location

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
 import React, {Component} from 'react';
 import MapView from 'react-native-maps';
+import isNil from "lodash/isNil"
 
 type Props = {};
 export default class App extends Component<Props> {
+
+  constructor(props) {
+    super(props)
+
+    const region = {
+      latitude: 38.778002,
+      longitude: -90.524456,
+      latitudeDelta: 0.9,
+      longitudeDelta: 0.9
+    }
+
+    this.state = { region }
+  }
+
   render() {
     return (
       <MapView
         style={{flex: 1}}
-        region={{
-          latitude: 42.882004,
-          longitude: 74.582748,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421
-        }}
+        region={this.state.region}
         showsUserLocation={true}
       />
     );
+  }
+
+  componentDidMount() {
+    const getPosition = (position) => {
+      if (isNil(position.coords)) { return }
+      const region = {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+        latitudeDelta: 0.2,
+        longitudeDelta: 0.2
+      }
+      this.setState({ region })
+      console.log("Region Set to :", region)
+    }
+
+    const watchPosition = (position) => {
+      console.log("New User Location:", position.coords)
+    }
+
+    navigator.geolocation.getCurrentPosition(getPosition)
+    navigator.geolocation.watchPosition(watchPosition)
   }
 }
